aea5b0bcc1226abe9dd94aa2e38d1e3e2d3a45cb
gpsign <gpsignorini@gmail.com>
2023-04-19 15:12:30 -0300

    + Add reset.css + Style tela 3 + Layout tela 3.1

---
+++ JS/script3.js
@@ -0,0 +1,22 @@
+const tela3Comeco = 
+`
+<div class="header-tela-3">
+        <p class="buzzquizz">BuzzQuizz</p>
+      </div>
+    <div class="tela-3">
+      <h1>Comece pelo comeco</h1>
+      <div class="caixa-inputs">
+        <input id="criar-input-titulo" type="text" placeholder="Título do seu quizz">
+        <input id="criar-input-url" type="text" placeholder="URL da imagem do seu quizz">
+        <input id="criar-input-nmrPerguntas" type="text" placeholder="Quantidade de perguntas do quizz">
+        <input id="criar-input-nmrNiveis" type="text" placeholder="Quantidade de níveis do quizz">
+      </div>
+      <button>Prosseguir pra criar perguntas</button>
+    </div>
+`
+
+function showTela3(){
+    document.querySelector("body").innerHTML = tela3Comeco;
+}
+
+showTela3();
