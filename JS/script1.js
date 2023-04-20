be51ab934420dfba2a3102b15ed5d9d9ea8acab5
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-04-20 16:15:06 -0300

    Tela 1 Mobile layout - Teamplate ready

--- JS/script1.js
+++ JS/script1.js
@@ -2,76 +2,46 @@ axios.defaults.headers.common['Authorization'] = 'M813n9erPvENXeuGPzKDL1Iu';
 
 const tela1 = `
 <section class="page1">
-<header>
-  BuzzQuizz
-</header>
-<main>
-  <div class="user-quizz">
-    <div class="no-quizz ">
-      <p>
-        Você não criou nenhum<br>quizz ainda :(
-      </p>
-      <button>Criar Quizz</button>
-    </div>
-    <div class="quizz-list hidden">
       <header>
-        <h1>Seus quizes</h1>
-        <ion-icon name="add-circle"></ion-icon>
+        BuzzQuizz
       </header>
-      <ul>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="all-quizz">
-    <div class="quizz-list">
-      <header>
-        <h1>Todos os Quizzes</h1>
-      </header>
-      <ul>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-        <li>
-          <img src="" alt="">
-          <h1>Acerte os personagens corretos dos Simpsons e prove seu amor!</h1>
-        </li>
-      </ul>
-  </div>
-</main>
-</section>
+      <main>
+        <div class="user-quizz">
+          <div class="no-quizz ">
+            <p>
+              Você não criou nenhum<br>quizz ainda :(
+            </p>
+            <button>Criar Quizz</button>
+          </div>
+          <div class="quizz-list hidden">
+            <header>
+              <h1>Seus quizes</h1>
+              <ion-icon name="add-circle"></ion-icon>
+            </header>
+            <ul>
+              <li>
+                <div class="gradient"></div>
+                <img src="" alt="">
+                <h1></h1>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="all-quizz">
+          <div class="quizz-list">
+            <header>
+              <h1>Todos os Quizzes</h1>
+            </header>
+            <ul>
+              <li>
+                <div class="gradient"></div>
+                <img src="" alt="">
+                <h1></h1>
+              </li>
+            </ul>
+        </div>
+      </main>
+    </section>
 `
 
 function showTela1 () {
