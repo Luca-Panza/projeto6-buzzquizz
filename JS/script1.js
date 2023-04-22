cf826d49896bba170568c2dec0a07c2d584aa729
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-04-22 18:53:45 -0300

    Quiz listing adjustments +data-test +code comments

--- JS/script1.js
+++ JS/script1.js
@@ -1,7 +1,8 @@
 axios.defaults.headers.common['Authorization'] = 'M813n9erPvENXeuGPzKDL1Iu';
 
-const tela1 = `
-<section class="page1">
+// Home page template/layout
+const tela1Template = `
+    <section class="page1">
       <header>
         BuzzQuizz
       </header>
@@ -11,12 +12,12 @@ const tela1 = `
             <p>
               Você não criou nenhum<br>quizz ainda :(
             </p>
-            <button>Criar Quizz</button>
+            <button data-test="create-btn">Criar Quizz</button>
           </div>
           <div class="quizz-list hidden">
             <header>
               <h1>Seus quizes</h1>
-              <ion-icon name="add-circle"></ion-icon>
+              <ion-icon name="add-circle" data-test="create-btn"></ion-icon>
             </header>
             <ul>
             </ul>
@@ -32,53 +33,56 @@ const tela1 = `
         </div>
       </main>
     </section>
-`
+`;
 
 function showTela1 () {
-  document.querySelector("body").innerHTML = tela1;
+  document.querySelector("body").innerHTML = tela1Template;
 }
-
-// showTela1();
 
 function getQuizz () {
   axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes')
   .then((res) => {
       console.log(res);
-      showTela1();
+      showTela1(); // displays the 
 
       const userQuizzList = document.querySelector('.user-quizz ul');
       const allQuizzList = document.querySelector('.all-quizz ul');
 
       const userLocalStorage = JSON.parse(localStorage.getItem('ids'));
-      let validUserQuiz = false;
-      console.log(userLocalStorage);
+      let validUserQuiz = false // used to keep track if there is any user quizz, if so, at the end of the function will display the USER QUIZ LIST 
       
       res.data.forEach((quiz) => {
-        if(userLocalStorage != null) {
-          userLocalStorage.forEach((item) => {
+        let userQuiz = false; // defines either the current quiz is going to be displayed as user quizz or not
+
+        if(userLocalStorage != null) { 
+          userLocalStorage.forEach((item) => { // check if the current quiz equals any of the local stored IDS
             if (quiz.id === item.id) {
-              validUserQuiz = true;
-              
-              userQuizzList.innerHTML += `
-                <li data-id='${quiz.id}'>
-                  <div class="gradient"></div>
-                  <img src="${quiz.image}" alt="">
-                  <h1>${quiz.title}</h1>
-                </li>
-              `;
+              userQuiz = true;
+              validUserQuiz = true; 
             } 
           });
         }
-        allQuizzList.innerHTML += `
-          <li data-id='${quiz.id}'>
+
+        if(userQuiz) {
+          userQuizzList.innerHTML += `
+          <li data-test="my-quiz" data-id='${quiz.id}'>
             <div class="gradient"></div>
             <img src="${quiz.image}" alt="">
             <h1>${quiz.title}</h1>
           </li>
         `;
+        } else {
+          allQuizzList.innerHTML += `
+            <li data-test="others-quiz" data-id='${quiz.id}'>
+              <div class="gradient"></div>
+              <img src="${quiz.image}" alt="">
+              <h1>${quiz.title}</h1>
+            </li>
+          `;
+        }
       })
 
-      if(validUserQuiz) {
+      if(validUserQuiz) { // displays the users quiz
         document.querySelector('.no-quizz').classList.add('hidden');
         document.querySelector('.user-quizz .quizz-list').classList.remove('hidden');
       }
